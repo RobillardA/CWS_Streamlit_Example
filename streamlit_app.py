12a16a41a0b419fed09332c9e172c13a3fde74c0
Alex Robillard, PhD <52674718+RobillardA@users.noreply.github.com>
2022-12-23 11:48:53 -0500

    Updates
    Changed file paths and text for new model deployment

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,14 +9,14 @@ from PIL import ImageOps
 import pathlib
 
 def main():
-    st.title('Peru Fish Classifier')
+    st.title('CWS Wildcat Example Classifier')
 
     for filename in EXTERNAL_DEPENDENCIES.keys():
         download_file(filename)
     
     model = load_model()
     
-    st.markdown("Fish photo for classification.")
+    st.markdown("Wildcat photo for classification.")
     image = st.file_uploader("", IMAGE_TYPES)
     if image:
         image_data = image.read()
@@ -103,9 +103,9 @@ def download_file(file_path):
 IMAGE_TYPES = ["png", "jpg"]
 
 EXTERNAL_DEPENDENCIES = {
-    "perumixed3.pkl": {
-        "url": "https://www.dropbox.com/s/31e6wuwrlm66sco/perumixed3.pkl?dl=1",
-        "size": 179319095
+    "example_cws_1.pkl": {
+        "url": "https://drive.google.com/file/d/1sgZPcOlssVjdlpRPYm9-8qRK2UEul5W5/view?usp=sharing",
+        "size": 179191808
     }
 }
 
